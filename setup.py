9758d520326aac0fafb1f44419cb549a65fc65db
Janneke Janssen <j.janssen@lukkien.com>
2012-07-02 12:10:13 +0200

    Added setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-p3p',
+    version='0.1',
+    description='Django P3P makes it easier to set P3P HTTP headers to prevent session loss.',
+    long_description=open('README.rst').read(),
+    author='Janneke Janssen',
+    author_email='j.janssen@lukkien.com',
+    url='http://github.com/jjanssen/django-p3p',
+    packages=find_packages(exclude=['p3p_example']),
+    include_package_data=True
+)
