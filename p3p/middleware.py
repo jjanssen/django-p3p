f15c9139533b404c3d99482ccabf85dd82454ad1
Janneke Janssen <j.janssen@lukkien.com>
2014-01-28 16:18:46 +0100

    Fixes issue #1, removed unused file and bumped version no.

--- p3p/middleware.py
+++ p3p/middleware.py
@@ -4,5 +4,5 @@ from django.template.loader import render_to_string
 class P3PMiddleware(object):
 
     def process_response(self, request, response):
-        response['P3P'] = render_to_string('p3p/headers.txt')
+        response['P3P'] = render_to_string('p3p/headers.txt').strip()
         return response
