343c7e7539a4aa585c5ee223bc3f750d4695d459
Janneke Janssen <j.janssen@lukkien.com>
2016-02-14 20:51:23 +0100

    No flake checking on test import work around

--- p3p/tests/__init__.py
+++ p3p/tests/__init__.py
@@ -1,6 +1,6 @@
 import django
 if django.VERSION < (1, 6):
     # Expose for Django 1.5 and below (before DiscoverRunner)
-    from .test_middleware import *
-    from .test_urls import *
-    from .test_views import *
+    from .test_middleware import *  # noqa
+    from .test_urls import *  # noqa
+    from .test_views import *  # noqa
