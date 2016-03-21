65f6009c2ff989921a8396d7498be65ba3c72b76
Janneke Janssen <j.janssen@lukkien.com>
2016-03-21 19:37:26 +0100

    Removed Django 1.5 check

--- p3p/tests/__init__.py
+++
@@ -1,6 +0,0 @@
-import django
-if django.VERSION < (1, 6):
-    # Expose for Django 1.5 and below (before DiscoverRunner)
-    from .test_middleware import *  # noqa
-    from .test_urls import *  # noqa
-    from .test_views import *  # noqa
